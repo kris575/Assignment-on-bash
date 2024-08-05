e8f8f60ad8fc271a8f4dad2d761c5b307a4c43e1
kris575 <obeyachristopher4@gmail.com>
2024-08-05 12:11:20 +0100

    this is my hero path

---
+++ Obeya.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Name : Obeya Christopher"
+echo "State of Origin: Benue State"
+echo "Gender: Male"
+echo "Primary School: LGEA Primary School Ogbaga Aidogodo Okpoga"
+echo "Secondary School: Emmaunel Secondary School Ugbokolo"
+echo "Favorite Meal: Beans and Plantain"
+echo "Favorite DevOps Instructor: Jethro Owoicho"
+
